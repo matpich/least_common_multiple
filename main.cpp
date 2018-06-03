42beda6cbe2d7593693769adf5a8a8af739f0d7e
Mateusz Pichniarczyk <mat.pich@interia.pl>
2018-06-03 19:54:12 +0200

    Working version.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 
 using namespace std;
+//greatest common divisor
+int gcd(int a, int b);
+
+// least common multiple
+int lcm(int a, int b);
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int first_num, second_num;
+
+    cout<<"This program finds \"least common multiple\" \nfor two given (int type) numbers."<<endl;
+    cout<<endl<<"First number: ";
+    cin>>first_num;
+
+    cout<<endl<<"Second number: ";
+    cin>>second_num;
+
+    cout<<endl<<"Least common multiple is: "<<lcm(first_num,second_num);
+
     return 0;
+}
+
+//euclidean algorithm
+int gcd(int a, int b)
+{
+    int c;
+    while(b!=0)
+    {
+        c=a%b;
+        a=b;
+        b=c;
+    }
+    return a;
+}
+
+
+int lcm(int a, int b)
+{
+    return (a*b)/gcd(a,b);
 }
